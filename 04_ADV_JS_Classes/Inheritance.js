1ad4cd1712310ef5eaaceb59fb4f12a54c1c4e33
octo9599 <felix.icloud.at@icloud.com>
2025-12-30 00:25:20 +0100

    ADV4 US2

---
+++ 04_ADV_JS_Classes/Inheritance.js
@@ -0,0 +1,32 @@
+class Animal {
+    name;
+    age;
+
+    constructor(name = 'name', age = 0) {
+        this.name = name;
+        this.age = age;
+    }
+
+    describe() {
+        return `${this.name} is ${this.age} years old`;
+    }
+}
+
+
+class Dog extends Animal {
+    breed;
+
+    constructor(name, age, breed="dog") {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    info() {
+        return super.describe() + ` and is of the breed ${this.breed}`;
+    }
+}
+
+
+const doggo = new Dog("Doggo", 2, "German Shepherd");
+console.log(doggo.describe());
+console.log(doggo.info());
