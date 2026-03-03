c704a3c932de3c9feebb02e9549ceb75595665fc
octo9599 <felix.icloud.at@icloud.com>
2026-03-03 16:37:04 +0100

    ADV 7

---
+++ 07_ADV_Linting_Debuging/src/TodoApp.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { ref, onMounted, watch } from 'vue'
+import TodoForm from './components/TodoForm.vue'
+import TodoItem from './components/TodoItem.vue'
+
+const todos = ref([]);
+
+onMounted(() => {
+    const saved = localStorage.getItem('todos');
+    if (saved) {
+        todos.value = JSON.parse(saved);
+    }
+})
+
+watch(todos, (newTodos) => {
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+}, 
+{ deep: true });
+
+function addTodo(text) {
+    todos.value.push({
+        id: Date.now(),
+        text
+    });
+}
+
+const removeTodo = (id) => {
+    todos.value = todos.value.filter(todo => todo.id !== id);
+}
+</script>
+
+<template>
+    <main class="app">
+        <h1>Vue 3 Todo App</h1>
+
+        <TodoForm @add-todo="addTodo" />
+
+        <p v-if="todos.length === 0" class="empty">
+            You have <strong>NO</strong> todos yet.
+        </p>
+
+        <TransitionGroup name="fade" tag="ul" class="todo-list">
+            <TodoItem v-for="todo in todos" :key="todo.id" @remove="removeTodo(todo.id)">
+                <template #default>
+                    {{ todo.text }}
+                </template>
+            </TodoItem>
+        </TransitionGroup>
+    </main>
+</template>
+
+<style>
+.app {
+    max-width: 500px;
+    margin: 2rem auto;
+    font-family: sans-serif;
+}
+
+.todo-list {
+    padding: 0;
+    list-style: none;
+}
+
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.5s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+
+p.empty {
+    margin-top: 2rem;
+    color: #bbb;
+    font-size: 1.2rem;
+    text-align: center;
+}
+</style>
