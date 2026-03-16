2c8858a27cafe1a9a498b6ee53cf47c03cf09891
octo9599 <felix.icloud.at@icloud.com>
2026-03-16 14:42:20 +0100

    Update CORE10/App.vue: removed old Version of code
    (first draft; didn't work properly)

--- 10_CORE_IP_Address_Lookup/IP-Lookup/src/App.vue
+++ 10_CORE_IP_Address_Lookup/IP-Lookup/src/App.vue
@@ -1,123 +1,9 @@
-<!--
 <script setup>
-import {onMounted, reactive, ref} from 'vue';
-import { watchDebounced } from '@vueuse/core';
-import axios from 'axios';
-
-const domain = ref("");
-const input = ref(null);
-
-onMounted(() => {
-	if (input.value) input.value.focus();
-});
-
-const dataObj = reactive({
-	ip: '',
-	country: '',
-	city: '',
-	coordinates: {
-		latitude: '',
-		longitude: '',
-	},
-	localTime: '',
-	timezone: '',
-	sunrise: '',
-	sunset: '',
-	currentWeather: {
-		temperature: '',
-		windSpeed: ''
-	}
-});
-
-watchDebounced(domain, async (newVal) => {
-
-	if (newVal.length > 3) {
-		await fetchIPData();
-		await Promise.all([
-			fetchSunTimes(dataObj.coordinates.latitude, dataObj.coordinates.longitude),
-			fetchWeather(dataObj.coordinates.latitude, dataObj.coordinates.longitude)
-		]);
-		console.log(dataObj);
-	}
-
-}, {debounce: 500});
-
-const fetchIPData = async () => {
-	try {
-		const httpApi = await axios.get(`http://ip-api.com/json/${domain.value}`);
-		const httpData = httpApi.data;
-		console.log(httpData);
-		dataObj.ip = httpData.query;
-		dataObj.country = httpData.country;
-		dataObj.city = httpData.city;
-		dataObj.coordinates.latitude = httpData.lat;
-		dataObj.coordinates.longitude = httpData.lon;
-		dataObj.timezone = httpData.timezone;
-		dataObj.localTime = new Date().toLocaleString('DE-de', { timeZone: httpData.timezone }).split(", ")[1];
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-const fetchSunTimes = async (lat, lon) => {
-	try {
-		const sunTimes = await axios.get(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}`);
-		const sunData = sunTimes.data;
-		console.log(sunData);
-		dataObj.sunset = sunData.results.sunset;
-		dataObj.sunrise = sunData.results.sunrise;
-	} catch (error) {
-		console.error(error);
-	}
-}
-
-const fetchWeather = async (lat, lon) => {
-	try{
-		const { data } = await axios.get('https://api.open-meteo.com/v1/forecast', {
-			params: {
-				latitude: lat,
-				longitude: lon,
-				current_weather: true
-			}
-		});
-		const weather = data;
-		dataObj.currentWeather.temperature = weather.current_weather.temperature;
-		dataObj.currentWeather.windSpeed = weather.current_weather.windspeed;
-	} catch (error) {
-		console.error(error);
-	}
-};
-</script>
-
-<template>
-	<input id="domain" ref="input" v-model="domain" autocomplete="off" name="domain" placeholder="Enter Domain"
-	       type="text">
-	<ul id="data-list" v-if="dataObj.ip && domain">
-		<li>IP-Address: {{ dataObj.ip }}</li>
-		<li>Country: {{ dataObj.country }}</li>
-		<li>City: {{ dataObj.city }}</li>
-		<li>Latitude: {{ dataObj.coordinates.latitude }}</li>
-		<li>Longitude: {{ dataObj.coordinates.longitude }}</li>
-		<li>Timezone: {{ dataObj.timezone }}</li>
-		<li>Local Time: {{ dataObj.localTime }}</li>
-		<li>Sunrise: {{ dataObj.sunrise }}</li>
-		<li>Sunset: {{ dataObj.sunset }}</li>
-		<li>Temperature: {{ dataObj.currentWeather.temperature }}˚C</li>
-		<li>Wind Speed: {{ dataObj.currentWeather.windSpeed }}kt</li>
-		&lt;!&ndash;<li>Local timezone: {{ data.timezone }}</li>
-		<li>Current Weather: {{ data.currentWeather }}</li>&ndash;&gt;
-	</ul>
-</template>
-
-<style scoped></style>
--->
-
-<script setup lang="ts">
 import { onMounted, reactive, ref } from 'vue';
 import { watchDebounced } from '@vueuse/core';
 import axios from 'axios';
 
-const domain = ref<string>('');
+const domain = ref('');
 const input = ref(null);
 const loading = ref(false);
 const error = ref('');
@@ -206,8 +92,7 @@ const fetchWeather = async (lat, lon) => {
 </script>
 
 <template>
-	<input id="domain" ref="input" v-model="domain" autocomplete="off" name="domain" placeholder="Enter Domain"
-	       type="text">
+	<input id="domain" ref="input" v-model="domain" autocomplete="off" name="domain" placeholder="Enter Domain" type="text">
 	<p v-if="loading">Loading...</p>
 	<p v-else-if="error">{{ error }}</p>
 	<ul id="data-list" v-else-if="dataObj.ip && domain">
